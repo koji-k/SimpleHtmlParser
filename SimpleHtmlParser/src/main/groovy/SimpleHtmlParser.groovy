31816c143a7180d1d48ca4535db29ffb7f04b5b1
koji.kuwana <saba.kann@gmail.com>
2013-02-15 20:11:43 +0100

    Initial Import

---
+++ SimpleHtmlParser/src/main/groovy/SimpleHtmlParser.groovy
@@ -0,0 +1,76 @@
+class SimpleHtmlParser {
+
+	def html
+	def tagType
+
+	def SimpleHtmlParser(List data){
+		html = data
+	}
+	def SimpleHtmlParser(String data){
+		this(data.readLines())
+	}
+
+	List parse(String startTag) {
+		def resultStack = []
+		def process = false
+		def counter = 0
+		def StringBuilder sb = new StringBuilder()
+		
+		setTagType(startTag)
+		html.each{ line ->
+
+			// store start
+			if( !process && line.indexOf(startTag) >= 0){ process = true }
+			if(process){
+				counter += countStartTag(line) - countEndTag(line)
+				sb.append("${line}\n")
+
+				// 開始タグに対応する終了タグを検知
+				if ( counter == 0 ){
+					process = false
+					resultStack.add(sb.toString())
+					sb.setLength(0)
+				}
+			}
+		}
+		resultStack
+	}
+
+	List parseToGPathResult(String startTag){
+		def result = parse(startTag)
+		result.collect({new XmlSlurper().parseText(it)})
+	}
+	
+	List parseToGPathResult(String startTag, Closure closure){
+		def slurpers = parseToGPathResult(startTag)
+		slurpers.each{ slurper ->
+			closure.call(slurper)
+		}
+	}
+	
+	// set first tag from target tag
+	// Z.b.
+	// <div class="a"> ---> div
+	// <div> ---> div
+	// <div /> ---> div
+	def setTagType(String targetTag){
+		//def match = (targetTag =~ /<([a-zA-Z]+)( .*>|>)/)
+		//def match = (targetTag =~ /<([a-zA-Z]+)(\s*.*)>/)
+		//def match = (targetTag =~ /<([a-zA-Z]+)([^>]*)>/)
+		def match = (targetTag =~ /<([a-zA-Z]+)([^>]*)>.*/)
+		if (match.matches()){
+			tagType = match[0][1]
+		}
+		tagType
+	}
+
+	def countStartTag(String line){
+		//line.findAll(~/<div/).size()
+		line.findAll(~/<${tagType}/).size()
+	}
+
+	def countEndTag(String line){
+		//line.findAll(~/<\/div/).size()
+		line.findAll(~/<\/${tagType}>/).size()
+	}
+}
---
+++ SimpleHtmlParser/src/test/groovy/SimpleHtmlParserTest.groovy
@@ -0,0 +1,51 @@
+import groovy.util.GroovyTestCase
+class SimpleHtmlParserTest extends GroovyTestCase{
+	def html = '''
+            <div class="dust">
+            </br >
+            <span class="target"><p>data1</p></span>
+            <span class="target"><p>data2</p></span>
+            <br />
+            <span class="target"><p>data3</p></span>
+            </div>
+        '''
+
+	def testTag = '''<span class="target">'''
+	
+	void testParse() {
+		def simpleHtmlParser = new SimpleHtmlParser(html)
+		def parseResult = simpleHtmlParser.parse(testTag)
+		assert "span" == simpleHtmlParser.tagType
+		assert 3 == parseResult.size()
+		assert '''<span class="target"><p>data1</p></span>''' == parseResult.get(0).trim()
+	}
+	
+	void testParseToGPathResult(){
+		def simpleHtmlParser = new SimpleHtmlParser(html)
+		def parseResult = simpleHtmlParser.parseToGPathResult(testTag)
+		def dev1 = parseResult.get(0)
+		def dev2 = parseResult.get(1)
+		def dev3 = parseResult.get(2)
+		assert dev1.@class == """target"""
+				assert dev1.p.text() == """data1"""
+		assert dev2.p.text() == """data2"""
+				assert dev3.p.text() == """data3"""
+	}
+	
+	void testParseToGpathResultWithClosure(){
+		def simpleHtmlParser = new SimpleHtmlParser(html)
+		def parseResult = simpleHtmlParser.parseToGPathResult(testTag)
+		StringBuilder sb = new StringBuilder()
+		simpleHtmlParser.parseToGPathResult(testTag){
+			sb.append(it.p.text())
+		}
+		assert "data1data2data3" == sb.toString()
+	}
+	
+	void testSetTagType() {
+		def simpleHtmlParser = new SimpleHtmlParser(html)
+		assert "div" == simpleHtmlParser.setTagType("<div>asdf</div>")
+		assert "div" == simpleHtmlParser.setTagType("<div class='hoge'></div>")
+		assert "div" == simpleHtmlParser.setTagType("<div /></div>")
+	}
+}
